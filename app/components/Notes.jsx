83af3967db53a51940c4931a682fc92fc2da38a5
cheryl <cherelle.blj@gmail.com>
2021-09-25 11:39:13 +0800

    generating ids using uuid

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import uuid from 'uuid';
 
 const notes = [
   {
-    id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
+    id: uuid.v4(),
     task: 'Learn React'
   },
   {
-    id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
+    id: uuid.v4(),
     task: 'Do laundry'
   }
 ];
@@ -16,3 +17,5 @@ export default () => (
     <li key={note.id}>{note.task}</li>
   )}</ul>
 )
+
+console.log(notes);
